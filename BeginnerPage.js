043c8d8e8ece0ff770ae37fd24fcf97bb91964f3
JJsrl <josharesco@gmail.com>
2024-12-11 12:29:14 -0500

    added gpt to beginner page

--- BeginnerPage.js
+++ BeginnerPage.js
@@ -1,16 +1,58 @@
 import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, Button, TouchableOpacity, View, TextInput } from 'react-native';
+import { ScrollView, StyleSheet, Text, Button, TouchableOpacity, View, TextInput, FlatList } from 'react-native';
+import Configuration from 'openai'
+import OpenAIApi from 'openai'
+
+const configuration = new Configuration({
+  apiKey: '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',
+})
+
+const openai = new OpenAIApi(configuration)
 
 const BeginnerPage = ({route, navigation}) => {
-  const [text, textChange] = React.useState('Type Here!')
+  const [input, setInput] = React.useState('Type Here!')
+  const [output, setOutput] = React.useState('')
+
+  const handleInput = async () => {
+    try {
+      const userInput = input
+      const response = await openai.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        {
+            role: "user",
+            content: userInput,
+        },
+    ],
+      temperature: 0,
+      max_tokens: 60,
+      top_p: 1.0,
+      frequency_penalty: 0.5,
+      presence_penalty: 0.0,
+      stop: ["You:"],
+
+      });
+      setOutput(response.choices[0].message.content);
+    } catch (error) {
+        console.log(error);
+    }
+
+    setInput('');
+  }  
+
+
     return (
       <ScrollView style = {styles.scrollcontainer}>
         <Text style = {styles.header}>Beginner Translator</Text>
         <Text style = {styles.text}>What would you like to ask?</Text>
-        <TextInput style = {styles.text} placeholder ={text} onChangeText={textChange}/>
-        <Button title = "Submit Answer" ></Button>
-        <Text>{text}</Text>
+        <TextInput style = {styles.text} placeholder ={input} onChangeText={setInput}/>
+        <TouchableOpacity onPress={handleInput}>
+          <Text style={styles.text}>Send</Text>
+        </TouchableOpacity>
+        <Text>{input}</Text>
+        <Text>{output}</Text>
         </ScrollView>
     )
     
